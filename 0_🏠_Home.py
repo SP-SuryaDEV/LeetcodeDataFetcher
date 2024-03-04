5a5a22295726e8a1bae038667e6741bd4afdc629
Valan-Tamil-Dasan <valantamildasan@gmail.com>
2024-03-04 11:48:25 +0530

    log

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
+import logging
 
-print('accessed')
+logging.basicConfig(filename='server_requests.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
+logging.info('Received request')
 st.session_state['data'] = pd.DataFrame()
 
 st.write('# **Welcome to Leetcode Data Fetcher!!**')
